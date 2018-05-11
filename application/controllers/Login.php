dc05c3ffa676576f7334e162b9f112544cafe9d4
AlexanderSid <alexandersidum@gmail.com>
2018-05-11 18:21:32 +0700

    Add files via upload

---
+++ application/controllers/Login.php
@@ -0,0 +1,48 @@
+<?php 
+ 
+class Login extends CI_Controller{
+ 
+	function __construct(){
+		parent::__construct();		
+		$this->load->model('m_login');
+ 
+	}
+ 
+	function index(){
+		$this->load->view('v_login');
+	}
+ 
+	function aksi_login(){
+		$username = $this->input->post('username');
+		$password = $this->input->post('password');
+		$where = array(
+			'username' => $username,
+			'password' => md5($password)
+			);
+		$cek = $this->m_login->cek_login("admin",$where)->num_rows();
+		if($cek > 0){
+ 
+			$data_session = array(
+				'nama' => $username,
+				'status' => "login"
+				);
+ 
+			$this->session->set_userdata($data_session);
+ 
+			$this->load->view('v_admin');
+ 
+		}else{
+			echo "Username dan password salah !";
+		}
+	}
+ 
+	function logout(){
+		$this->session->sess_destroy();
+		redirect(base_url('login'));
+	}
+
+		function admin(){
+		$this->load->view('v_admin');
+	}
+
+}
