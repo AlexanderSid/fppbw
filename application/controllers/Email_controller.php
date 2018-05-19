dff1b3cac89f0d384d8dbb09e3f24b6879a0213d
AlexanderSid <alexandersidum@gmail.com>
2018-05-20 01:21:11 +0700

    Update Email_controller.php

--- application/controllers/Email_controller.php
+++ application/controllers/Email_controller.php
@@ -37,8 +37,33 @@
       }*/
       public function sendmsg(){
         $nama = $this->input->post('nama');
-        $email = $this->input->post('email');
+        $imail = $this->input->post('email');
         $pesan = $this->input->post('pesan');
+        $able= true;
+
+        if (empty($nama)) {
+        $this->session->set_flashdata('b', '0');
+        $able= false;
+        }
+
+        if (empty($imail)) {
+        $this->session->set_flashdata('c', '0');
+        $able= false;
+        }
+        else{
+          if (!filter_var($imail, FILTER_VALIDATE_EMAIL)) {
+          $this->session->set_flashdata('c', '2');
+          $able= false;
+        }
+        }
+
+        if (empty($pesan)) {
+        $this->session->set_flashdata('d', '0');
+        $able= false;
+        }
+
+        
+
 
         $config = array(
          'protocol' => 'smtp',
@@ -49,30 +74,48 @@
          'mailtype' => 'html',
          'charset' => 'iso-8859-1',
          'wordwrap' => TRUE
-      );
+        );
 
+       if ($able==true) {
+          
        $this->load->library('email', $config);
        $this->email->set_newline("\r\n");
        $this->email->from($config['smtp_user']); // change it to yours
        $this->email->to('furrieslord01@gmail.com');// change it to yours
-       $this->email->subject("Pesan dari ", $email, "bernama ",$nama);
+       $this->email->subject("Equitas - Pesan dari ".$email." - ".$nama);
        $this->email->message($pesan);
        if($this->email->send()){
-         $this->session->set_flashdata('message', 'Email sent');
+         $this->session->set_flashdata('kod', '1');
        }
        else{
-         $this->session->set_flashdata('message', show_error($this->email->print_debugger()));
- 
+         $this->session->set_flashdata('kod', '0');
+       }}
+       else{
+          $this->session->set_flashdata('kod', '0');
        }
+       redirect('main/contact');
+
       }
 
       public function sendemail(){
       $subject = $this->input->post('subject');
       $message = $this->input->post('isi');
+      $able=true;
 
+      if (empty($subject)) {
+        $this->session->set_flashdata('w', '0');
+        $able= false;
+        }
+
+      if (empty($message)) {
+        $this->session->set_flashdata('e', '0');
+        $able= false;
+        }
+
+
+       if ($able==true) {
+          
       $conn = mysqli_connect("localhost","root","","subscriber");
-
-
       $sql = "SELECT email FROM email";
       $result = $conn->query($sql) or die(mysql_error());
       while($row = mysqli_fetch_array($result))
@@ -99,12 +142,20 @@
        $this->email->subject($subject);
        $this->email->message($message);
        if($this->email->send()){
-         $this->session->set_flashdata('message', 'Email sent');
+         $this->session->set_flashdata('q', '1');
        }
        else{
-         $this->session->set_flashdata('message', show_error($this->email->print_debugger()));
+         $this->session->set_flashdata('q', '0');
  
+       }
+       }
+       else{
+
+        $this->session->set_flashdata('q', '0');
+        
        } 
+
+
        redirect('Email_controller');
  
    } 
