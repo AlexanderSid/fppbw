acc72d39cefced6459e4bc4cce3399d378525b8a
AlexanderSid <alexandersidum@gmail.com>
2018-05-20 01:35:42 +0700

    Update v_about.php

--- application/views/v_about.php
+++ application/views/v_about.php
@@ -13,7 +13,14 @@
 
     <!-- Bootstrap core CSS -->
     <link href="<?php echo base_url();?>assets/css/admin/bootstrap/css/bootstrap.min.css" rel="stylesheet">
-
+    <!-- custom core CSS -->
+        <link href="<?php echo base_url();?>assets/css/heroic-features.css" rel="stylesheet">
+    <link href="<?php echo base_url();?>assets/css/slideshow.css" rel="stylesheet">
+        <link href="<?php echo base_url();?>assets/lib/font-awesome/css/font-awesome.min.css" rel="stylesheet">
+    <link href="<?php echo base_url();?>assets/lib/animate/animate.min.css" rel="stylesheet">
+    <link href="<?php echo base_url();?>assets/lib/ionicons/css/ionicons.min.css" rel="stylesheet">
+    <link href="<?php echo base_url();?>assets/lib/owlcarousel/assets/owl.carousel.min.css" rel="stylesheet">
+    <link href="<?php echo base_url();?>assets/lib/lightbox/css/lightbox.min.css" rel="stylesheet">
   <style >
   .display-6 {
   font-size: 2rem;
@@ -40,10 +47,214 @@
     margin-right: auto;
     width: 50%;
 }
+/* About Section
+--------------------------------*/
+#about {
+  padding: 60px 0 30px 0;
+}
+
+#about .about-img {
+  overflow: hidden;
+}
+
+#about .about-img img {
+  margin-left: -15px;
+  max-width: 100%;
+}
+
+@media (max-width: 768px) {
+  #about .about-img {
+    height: auto;
+  }
+  #about .about-img img {
+    margin-left: 0;
+    padding-bottom: 30px;
+  }
+}
+
+#about .content h2 {
+  color: #d80404;
+  font-weight: 700;
+  font-size: 32px;
+  font-family: "Raleway", sans-serif;
+}
+
+#about .content h3 {
+  color: #555;
+  font-weight: 300;
+  font-size: 18px;
+  line-height: 26px;
+  font-style: italic;
+}
+
+#about .content p {
+  line-height: 26px;
+}
+
+#about .content p:last-child {
+  margin-bottom: 0;
+}
+
+#about .content i {
+  font-size: 20px;
+  padding-right: 4px;
+  color: #50d8af;
+}
+
+#about .content ul {
+  list-style: none;
+  padding: 0;
+}
+
+#about .content ul li {
+  padding-bottom: 10px;
+}
+
+/*--------------------------------------------------------------
+# Footer
+--------------------------------------------------------------*/
+#footer {
+  background: #a30404;
+  padding: 0 0 30px 0;
+  color: #eee;
+  font-size: 14px;
+}
+
+#footer .footer-top {
+  background: #a30404;
+  padding: 60px 0 30px 0;
+}
+
+#footer .footer-top .footer-info {
+  margin-bottom: 30px;
+}
+
+#footer .footer-top .footer-info h3 {
+  font-size: 34px;
+  margin: 0 0 20px 0;
+  padding: 2px 0 2px 10px;
+  line-height: 1;
+  font-family: "Montserrat", sans-serif;
+  font-weight: 700;
+  letter-spacing: 3px;
+  border-left: 4px solid #18d26e;
+}
+
+#footer .footer-top .footer-info p {
+  font-size: 14px;
+  line-height: 24px;
+  margin-bottom: 0;
+  font-family: "Montserrat", sans-serif;
+  color: #eee;
+}
+
+
+
+#footer .footer-top h4 {
+  font-size: 14px;
+  font-weight: bold;
+  color: #fff;
+  text-transform: uppercase;
+  position: relative;
+  padding-bottom: 12px;
+}
+
+#footer .footer-top h4::before, #footer .footer-top h4::after {
+  position: absolute;
+  left: 0;
+  bottom: 0;
+  height: 2px;
+}
+
+#footer .footer-top h4::before {
+  right: 0;
+  background: #555;
+}
+
+#footer .footer-top h4::after {
+  background: #18d26e;
+  width: 60px;
+}
+
+#footer .footer-top .footer-links {
+  margin-bottom: 30px;
+}
+
+#footer .footer-top .footer-links ul {
+  list-style: none;
+  padding: 0;
+  margin: 0;
+}
+
+#footer .footer-top .footer-links ul i {
+  padding-right: 8px;
+  color: #ddd;
+}
+
+#footer .footer-top .footer-links ul li {
+  border-bottom: 0px solid #333;
+  padding: 10px 0;
+}
+
+#footer .footer-top .footer-links ul li:first-child {
+  padding-top: 0;
+}
+
+#footer .footer-top .footer-links ul a {
+  color: #eee;
+}
+
+#footer .footer-top .footer-links ul a:hover {
+  color: #330303;
+}
+
+#footer .footer-top .footer-contact {
+  margin-bottom: 30px;
+}
+
+#footer .footer-top .footer-contact p {
+  line-height: 26px;
+}
+
+#footer .footer-top .footer-newsletter {
+  margin-bottom: 30px;
+}
+
+#footer .footer-top .footer-newsletter input[type="email"] {
+  border: 0;
+  padding: 6px 8px;
+  width: 65%;
+}
+
+#footer .footer-top .footer-newsletter input[type="submit"] {
+  background: #18d26e;
+  border: 0;
+  width: 35%;
+  padding: 6px 0;
+  text-align: center;
+  color: #fff;
+  transition: 0.3s;
+  cursor: pointer;
+}
+
+#footer .footer-top .footer-newsletter input[type="submit"]:hover {
+  background: #13a456;
+}
+
+#footer .copyright {
+  text-align: center;
+  padding-top: 30px;
+}
+
+#footer .credits {
+  text-align: center;
+  font-size: 13px;
+  color: #ddd;
+}
+
   </style>
     <!-- Custom styles for this template -->
-    <link href="<?php echo base_url();?>assets/css/heroic-features.css" rel="stylesheet">
-    <link href="<?php echo base_url();?>assets/css/slideshow.css" rel="stylesheet">
+  <link href="<?php echo base_url();?>assets/css/cssnew.css" rel="stylesheet">
   </head>
 
   <body>
@@ -80,30 +291,38 @@
     <div class="container">
     
       <!-- Jumbotron Header -->
-
-      <h1 class="display-6 text-center">About Us</h1>
-      <header class="jumbotron">        
-        <img class="center"src="<?php echo base_url('assets/img/eq3.png');?>" >
-        <p class="lead">Equitas adalah tabloid yang mulai didirikan sejak tahun .... oleh bpk Antonius,
-        blablablablablabla blablablablablablablablablablablablablablabla blablablablablablablablabla</p>
-        
-      </header>
+    <section id="about" class="wow fadeInUp">
+      <div class="container">
+        <div class="row">
+          <div class="col-lg-6 about-img">
+            <img src="<?php echo base_url('assets/img/eqbg.jpg');?>" alt="">
+          </div>
 
 
+          <div class="col-lg-6 content">
+            <h2>Apa itu tabloid Equitas?</h2>
+            <h3>Tabloid pewarta hukum dan politik se Malang-raya dan Nusa Tenggara Timur berslogan "Justice for all"</h3>
+
+            <h2>Apa makna dari "Justice for all"?</h2>
+            <h3>Justice for all merupakan slogan yang kami perjuangkan sejak awal pendirian equitas, yang mana dengan adanya tabloid ini kami berharap keterbukaan hukum dan politik khususnya pada daerah Malang-raya dan NTT mulai meningkat dan secara langsung maupun tidak langsung akan menambah pengetahuan masyarakat demi keadilan yang merata</h3>
+            <br>
+            <p><b>Sejarah Singkat</b></p>
+            <ul>
+              <li><i class="ion-android-checkmark-circle" style="color: #d80404"></i> Tahun berdiri : 2011</li>
+              <li><i class="ion-android-checkmark-circle" style="color: #d80404"></i> Lokasi berdiri : Malang - Jawa Timur</li>
+              <li><i class="ion-android-checkmark-circle" style="color: #d80404"></i> Pendiri : Antonius Gago S.L , Hery Lumba</li>
+            </ul>
+            <img src="assets/img/eq2.png" alt="">
+          </div>
+        </div>
+
+      </div>
+    </section>
 
      
 
-    <!-- Footer -->
-    <footer class="py-5 bg-dark">
-      <div class="container">
-        <p class="m-0 text-center text-white"></p>
-        
-      </div>
-      <div id=link-container>
-      <a class="text-white" href="<?php echo site_url('login'); ?>">Masuk</p>
-      </div>
-      <!-- /.container -->
-    </footer>
+
+
 
     <!-- Bootstrap core JavaScript -->
     <script src="<?php echo base_url();?>assets/vendor/jquery/jquery.min.js"></script>
